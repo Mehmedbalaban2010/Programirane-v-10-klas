0badf08b4147233af207fded69746af73c2c24d9
Mehmedbalaban2010 <mehmed.balaban@paisii.info>
2026-09-18 14:54:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Programa
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[5];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
